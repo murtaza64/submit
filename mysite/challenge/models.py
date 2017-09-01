96f14673518611133208d39f7ed15800b1cd864c
Murtaza Javaid <murtaza.javaid@gmail.com>
2017-09-01 07:45:01 -0400

    added more to models

--- mysite/challenge/models.py
+++ mysite/challenge/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db.models import Model, CharField, DateField, TextField, IntegerField, ForeignKey, \
-OneToOneField, ManyToManyField
+OneToOneField, ManyToManyField, DateTimeField, BooleanField
+import datetime
 
 class Challenge(models.Model):
     title = CharField(max_length=140)
     subject = ForeignKey("Subject")
-    body = TextField(null=True)
+    body = TextField()
+    start = DateTimeField(auto_now_add=True)
+    deadline = DateTimeField(default=datetime.datetime(2100,1,1))
+    correct_answers = ManyToManyField("Answer")
+    auto_check = BooleanField(default=True)
+    solution_only = BooleanField(default=False)
+
+class Answer(models.Model):
+    value = CharField(max_length=140)
 
 class Subject(models.Model):
     name = CharField(max_length=30)
@@ -18,4 +27,6 @@ class StudentProfile(models.Model):
 class ChallengeAttempt(models.Model):
     student = ForeignKey(StudentProfile)
     challenge = ForeignKey(Challenge)
-    date = DateField(auto_now=True)
+    date = DateField(auto_now_add=True)
+    answer = CharField(max_length=140, null=True)
+    solution = TextField(null=True)
