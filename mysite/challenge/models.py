7bbed8de89fd989160504fc61c6a97d359664850
Murtaza Javaid <murtaza.javaid@gmail.com>
2017-08-31 22:23:58 -0400

    added initial models and migrated db

--- mysite/challenge/models.py
+++ mysite/challenge/models.py
@@ -1,5 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models import Model, CharField, DateField, TextField, IntegerField, ForeignKey, \
+OneToOneField, ManyToManyField
 
-# Create your models here.
+class Challenge(models.Model):
+    title = CharField(max_length=140)
+    subject = ForeignKey("Subject")
+    body = TextField(null=True)
 
-model()
+class Subject(models.Model):
+    name = CharField(max_length=30)
+
+class StudentProfile(models.Model):
+    user = OneToOneField(User)
+    subjects = ManyToManyField(Subject)
+
+class ChallengeAttempt(models.Model):
+    student = ForeignKey(StudentProfile)
+    challenge = ForeignKey(Challenge)
+    date = DateField(auto_now=True)
