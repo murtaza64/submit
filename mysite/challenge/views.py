9aeb8a6d9cf52a517f0a68f3d1be29c9c4ce9060
Murtaza Javaid <murtaza.javaid@gmail.com>
2017-09-03 14:51:01 -0400

    changed challenge view to a single function

--- mysite/challenge/views.py
+++ mysite/challenge/views.py
@@ -2,21 +2,26 @@ from django.shortcuts import render, redirect
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from .models import Challenge, ChallengeAttempt
+from .forms import ChallengeAttemptForm
 
 # Create your views here.
 
-class ChallengeView(DetailView):
-    model = Challenge
-    template_name = "challenge/challenge.html"
+# class ChallengeView(DetailView):
+#     model = Challenge
+#     template_name = "challenge/challenge.html"
 
 class ChallengeListView(ListView):
     model = Challenge
     template_name = "challenge/challenge_list.html"
     
 def submit_challenge(request, pk):
+    obj = Challenge.objects.get(pk=pk)
     if request.method == "POST":
-        print("hello world")
         print(request.POST)
-        return redirect('/')
+        form = ChallengeAttemptForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            return redirect('/')
     else:
-        return ridrect('/challenge/pk')
+        form = ChallengeAttemptForm()
+    return render(request, 'challenge/challenge.html', {'form': form, 'challenge': obj})
